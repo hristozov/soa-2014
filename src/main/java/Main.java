45d3b6ee511e603fb71ab0583f779695ce70f102
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-31 22:12:02 +0300

    Allowing single-element arrays in produced JSON.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ import service.CurrencyRateService;
 import service.FoodService;
 import service.OrderService;
 import service.UserService;
+import transport.JsonMessageBodyWriter;
 
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class Main {
 		Injector injector = Guice.createInjector(new ServletModule() {
 			@Override
 			protected void configureServlets() {
+				this.bind(JsonMessageBodyWriter.class);
 			}
 		});
 		ResourceConfig rc = new PackagesResourceConfig("service");
